4f587b056fbb26cde7dab7ceb587c60b1a05fa67
yushu <591821761@qq.com>
2021-01-07 11:31:53 +0800

    将解析后的数据录入到线程变量

--- src/main/java/com/yushu/flume/PolluteSinker.java
+++ src/main/java/com/yushu/flume/PolluteSinker.java
@@ -3,15 +3,20 @@ package com.yushu.flume;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.yushu.flume.entity.Pollute;
+import com.yushu.flume.entity.ReceiveData;
 import com.yushu.flume.util.FormatDataUtil;
+import com.yushu.flume.util.MapObjectTrans;
 import org.apache.flume.*;
 import org.apache.flume.conf.Configurable;
 import org.apache.flume.sink.AbstractSink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.NamedThreadLocal;
+
 import java.nio.charset.Charset;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,6 +29,10 @@ import java.util.Map;
 public class PolluteSinker extends AbstractSink implements Configurable {
 
     private Logger LOG = LoggerFactory.getLogger(PolluteSinker.class);
+
+    //用来存储解析后的数据
+    private static ThreadLocal<Map<String,String>> myThreadLocal = new NamedThreadLocal<>("My ThreadLocal");
+
 
     private String tableName;
     private String user;
@@ -78,8 +87,37 @@ public class PolluteSinker extends AbstractSink implements Configurable {
                     LOG.info("--- content : " + temp);
                     System.out.println(temp);
                     map = FormatDataUtil.getMapByData(temp);
-                    List<Pollute> pollutes = new ArrayList<>();
+
                     for (Map.Entry<String, String> entry : map.entrySet()) {
+                        PolluteSinker.addValueToMyThreadLocal(entry.getKey(),entry.getValue());
+                    }
+                    ReceiveData data = new ReceiveData();
+                    data.setEvent_id(PolluteSinker.getValueToMyThreadLocal("event_id"));
+                    data.setCreate_time(PolluteSinker.getValueToMyThreadLocal("create_time"));
+                    data.setFile_name(PolluteSinker.getValueToMyThreadLocal("file_name"));
+                    data.setFile_path(PolluteSinker.getValueToMyThreadLocal("file_path"));
+                    data.setContent_text(PolluteSinker.getValueToMyThreadLocal("content_text"));
+                    data.setContent_text(PolluteSinker.getValueToMyThreadLocal("key_word"));
+
+                    PolluteSinker.destroy();
+
+                    if (data != null){
+                        preparedStatement.clearBatch();
+                        preparedStatement.setString(1,data.getEvent_id());
+                        preparedStatement.setString(2,data.getCreate_time());
+                        preparedStatement.setString(3,data.getFile_name());
+                        preparedStatement.setString(4,data.getFile_path());
+                        preparedStatement.setString(5,data.getContent_text());
+                        preparedStatement.setString(6,data.getKey_word());
+                        preparedStatement.addBatch();
+                        preparedStatement.executeBatch();
+                        conn.commit();
+
+                    }
+
+                    /*List<Pollute> pollutes = new ArrayList<>();
+                    for (Map.Entry<String, String> entry : map.entrySet()) {
+
                         Pollute pollute = new Pollute();
                         pollute.setKey(entry.getKey());
                         pollute.setValue(entry.getValue());
@@ -95,7 +133,7 @@ public class PolluteSinker extends AbstractSink implements Configurable {
                         }
                         preparedStatement.executeBatch();
                         conn.commit();
-                    }
+                    }*/
                 }
             }
             transaction.commit();
@@ -104,6 +142,7 @@ public class PolluteSinker extends AbstractSink implements Configurable {
             LOG.error("", e);
         } finally {
             if (transaction != null) {
+
                 transaction.close();
             }
 
@@ -129,7 +168,7 @@ public class PolluteSinker extends AbstractSink implements Configurable {
             conn = DriverManager.getConnection(url, user, password);
             conn.setAutoCommit(false);
             //创建一个Statement对象
-            preparedStatement = conn.prepareStatement("insert into bs_pollute(`key`,`value`,date_add) values (?,?,?)");
+            preparedStatement = conn.prepareStatement("insert into sp_receive_data(`event_id`,`create_time`,`file_name`,`file_path`,`content_text`,`key_word`) values (?,?,?,?,?,?)");
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -164,5 +203,50 @@ public class PolluteSinker extends AbstractSink implements Configurable {
         }
 
     }
+
+
+
+    /**
+     * 添加值到线程变量
+     *
+     * @param key 存储的key
+     * @param value 对应的值
+     */
+    public static void addValueToMyThreadLocal(String key,Object value){
+        Map<String,String> map = myThreadLocal.get();
+        if (map == null){
+            map = new HashMap<>();
+        }
+        map.put(key, (String) value);
+        myThreadLocal.set(map);
+    }
+
+    /**
+     * 获取线程变量的值
+     *
+     * @param key 存储的key
+     * @return
+     */
+    public static String getValueToMyThreadLocal(String key){
+        return myThreadLocal.get().get(key);
+    }
+
+
+
+    /**
+     * 销毁该线程变量
+     */
+    public static void destroy() {
+        myThreadLocal.remove();
+    }
+
+
+
+
+
+
+
+
+
 
 }
