7a4629f52a153b1587038050af21402a9b9cce0e
yushu <591821761@qq.com>
2021-01-11 13:48:10 +0800

    处理消息Event(本例写入数据到redis)

--- src/main/java/com/yushu/flume/RedisSinker.java
+++ src/main/java/com/yushu/flume/RedisSinker.java
@@ -1,14 +1,19 @@
 package com.yushu.flume;
 
-
+import com.google.common.collect.Lists;
+import org.apache.flume.*;
 import com.google.common.base.Preconditions;
+import org.apache.flume.Channel;
 import org.apache.flume.Context;
 import org.apache.flume.EventDeliveryException;
+import org.apache.flume.Transaction;
 import org.apache.flume.conf.Configurable;
 import org.apache.flume.sink.AbstractSink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
+import java.nio.charset.Charset;
+import java.util.List;
 
 /**
  *
@@ -66,19 +71,64 @@ public class RedisSinker extends AbstractSink implements Configurable {
     }
 
 
-
+    /**
+     * 不断循环调用,处理消息Event(本例写入数据到redis)
+     *
+     * @return
+     * @throws EventDeliveryException
+     */
     @Override
     public Status process() throws EventDeliveryException {
 
+        Status result = Status.READY;
+        Channel channel = getChannel();
+        List<String> actions = Lists.newArrayList();
+        Transaction transaction = null;
+
+        try {
+
+            transaction = channel.getTransaction();
+            Event event;
+            String content;
+            transaction.begin();//开启事务
+            event = channel.take();
+            logger.info("event:{}",event);
+            if (event != null){
+                System.out.println("content++++:"+event.getBody());
+                content = new String(event.getBody(), Charset.forName("UTF-8"));
+                logger.info("content value:" + event.getBody());
+                actions.add(content);
+
+            }else {
+                result = Status.BACKOFF;
+            }
+
+            if (actions.size() > 0){
+
+                for (String temp : actions){
+                    logger.info("temp value:" + temp);
+                    /**
+                     * 日志解析存到redis上
+                     *
+                     */
+
+                }
+                transaction.commit();
 
 
 
+            }
 
 
+        }catch (Throwable  e){
+            logger.error("fail to show");
+            transaction.rollback();
 
+        }finally {
+            transaction.close();
+        }
 
-
-        return null;
+        return result;
     }
 
 
